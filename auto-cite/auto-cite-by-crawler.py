4d374379e2b4d745a357573325298fb93fe56dcd
Deriq-Qian-Dong <471607065@qq.com>
2023-11-01 01:12:58 +0800

    google_crawler

---
+++ auto-cite/auto-cite-by-crawler.py
@@ -0,0 +1,168 @@
+from util import *
+from importlib import import_module
+from dict_hash import sha256
+
+# config info for input/output files and plugins
+config = {}
+try:
+    config = load_data("../_config.yaml", type_check=False).get("auto-cite")
+    if not config:
+        raise Exception("Couldn't find auto-cite key in config")
+except Exception as e:
+    log(e, 3, "red")
+    exit(1)
+
+log("Compiling list of sources to cite")
+
+# compile master list of sources from various plugins
+sources = []
+
+# error exit flag
+will_exit = False
+
+# loop through plugins
+for plugin in config.get("plugins", []):
+    # get plugin props
+    name = plugin.get("name", "[no name]")
+    files = plugin.get("input", [])
+
+    # show progress
+    log(f"Running {name} plugin")
+
+    # loop through plugin input files
+    for file in files:
+        # show progress
+        log(file, 2)
+
+        plugin_sources = []
+        try:
+            # get data in file
+            data = load_data(file)
+            # run plugin
+            plugin_sources = import_module(f"plugins.{name}").main(data)
+        except Exception as e:
+            log(e, 3, "red")
+            will_exit = True
+
+        log(f"Got {len(plugin_sources)} sources", 2, "green")
+
+        for source in plugin_sources:
+            # include meta info about plugin and source
+            source["_plugin"] = name
+            source["_input"] = file
+            # make unique key for cache matching
+            source["_cache"] = sha256(source)
+            # add source
+            sources.append(source)
+
+# exit at end of loop if error occurred
+if will_exit:
+    log("One or more input files or plugins failed", 3, "red")
+    exit(1)
+
+log("Generating citations for sources")
+
+# load existing citations
+citations = []
+try:
+    citations = load_data(config["output"])
+except Exception as e:
+    log(e, 2, "yellow")
+
+# error exit flag
+will_exit = False
+
+# list of new citations to overwrite existing citations
+new_citations = []
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
+from time import sleep
+from selenium.webdriver.common.by import By
+from tqdm import tqdm
+def setup_webdriver():
+    # setup webdriver
+    from selenium import webdriver
+    from webdriver_manager.chrome import ChromeDriverManager
+    from webdriver_manager.utils import ChromeType
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
+
+    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+    chrome_options = Options()
+    options = [
+        "--headless",
+        "--disable-gpu",
+        "--window-size=1920,1200",
+        "--ignore-certificate-errors",
+        "--disable-extensions",
+        "--no-sandbox",
+        "--disable-dev-shm-usage"
+    ]
+    for option in options:
+        chrome_options.add_argument(option)
+
+    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    return driver
+
+wd = setup_webdriver()
+publications={}
+supervisors = ['https://scholar.google.com/citations?user=NJOnxh4AAAAJ&hl=en','https://scholar.google.com/citations?user=0HtCYQEAAAAJ&hl=en','https://scholar.google.com/citations?user=UKqaI5IAAAAJ&hl=en']
+for supervisor in supervisors:
+    depth = config.get("depth", 10)
+    wd.get(supervisor)
+    wd.find_element(By.XPATH, '//*[@id="gsc_a_ha"]/a').click()
+    for i in range(3):
+        wd.find_element(By.XPATH, '//*[@id="gsc_bpf_more"]/span/span[2]').click()
+        sleep(3)
+
+    for i in tqdm(range(depth)):
+        part = wd.find_element(By.XPATH, f'//*[@id="gsc_a_b"]/tr[{i+1}]/td[1]/a')
+        href = part.get_attribute('href')
+        sleep(1)
+        wd.execute_script(f"window.open('{href}', 'new_tab')")
+        wd.switch_to.window("new_tab")
+        title = wd.find_element(By.ID, 'gsc_oci_title').text
+        author = wd.find_element(By.CLASS_NAME, 'gsc_oci_value').text
+        date = wd.find_element(By.XPATH, '//*[@id="gsc_oci_table"]/div[2]/div[2]').text
+        pub = wd.find_element(By.XPATH, '//*[@id="gsc_oci_table"]/div[3]/div[2]').text
+        cnt = 4
+        while True:
+            try:
+                url = wd.find_element(By.XPATH, f'//*[@id="gsc_oci_table"]/div[{cnt}]/div[2]/div/div[1]/a').get_attribute("href")
+                break
+            except:
+                cnt+=1
+        publications[title] = {'title':title, 'authors':[a.strip() for a in author.split(",")], 'publisher':pub, 'date':date, 'link':url}
+        sleep(1)
+        wd.close()
+        wd.switch_to.window(wd.window_handles[0])
+        
+for pub in citations:
+    title = pub["title"]
+    if 'id' in pub:
+        pub.pop("id")
+    if '_cache' in pub:
+        pub.pop("_cache")
+    publications[title] = pub
+
+new_citations = []
+for title in publications:
+    new_citations.append({'id':title, 'title':title, "authors":publications[title]['authors'], "publisher":publications[title]['publisher'], "date":publications[title]['date'], "link":publications[title]['link']})
+ 
+# exit at end of loop if error occurred
+if will_exit:
+    log("One or more sources failed to be cited", 3, "red")
+    exit(1)
+
+log("Exporting citations")
+
+# save new citations
+try:
+    save_data(config["output"], new_citations)
+except Exception as e:
+    log(e, 2, "red")
+    exit(1)
+
+log(f"Exported {len(new_citations)} citations", 2, "green")
